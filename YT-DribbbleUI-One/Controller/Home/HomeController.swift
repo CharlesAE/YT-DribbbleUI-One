b5ebbfc4aa80ec1e872d0cd0eee3fd677c74a5a2
CharlesAE <charles.ad.edwards@gmail.com>
2023-10-10 17:57:43 -0400

    Update README.md

--- YT-DribbbleUI-One/Controller/Home/HomeController.swift
+++ YT-DribbbleUI-One/Controller/Home/HomeController.swift
@@ -12,7 +12,6 @@ class HomeController: UICollectionViewController {
 
 let headerCell = "HeaderCell"
 let bodyCell = "BodyCell"
-//products array, from the Product model object
 let products = Product.products
 override func viewDidLoad() {
       super.viewDidLoad()
@@ -38,6 +37,7 @@ override func viewDidLoad() {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
     
     //let collectionview know that it will have 2 sections now
     // first section is the header
@@ -58,6 +58,18 @@ override func viewDidLoad() {
         {
         return products.count
         }
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let product: Product?
+        let detailController = Details()
+        if indexPath.section == 1 {
+            
+            product = products[indexPath.item]
+            detailController.product = product
+        }
+        
+        navigationController?.pushViewController(detailController, animated: true)
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
